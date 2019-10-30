5d17f826717031643b39d50be8174f3d3fcd46ec
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-2.local>
2019-10-30 12:48:04 -0400

    removed attays, just working with strings

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -7,33 +7,26 @@ public class Palindrome {
     //find all possiblesubStrings and just test them as palindromes
     public Integer countPalindromes(String input) {
         int count = input.length();
-        List<String> substringArray = allPossibleSubstrings(input);
-        if(isPalindrome(substringArray))
-            count++;
-
-
-        return count;
-    }
-    public boolean isPalindrome(List<String> substringArray) {
-        //create a string to reverse
-        ArrayList reversedString = new ArrayList();
-        int valueCounter = 0;
-        for (int i = substringArray.size() - 1; i >= 0; i--) {
-            reversedString.add(substringArray.get(valueCounter));
-            valueCounter++;
-        }
-        return substringArray.equals(reversedString);
-    }
-
-    public List<String> allPossibleSubstrings(String input){
-        List<String> allSubStrings = new ArrayList<>();
         String tempSubString = "";
-        for(int i = 0; i <= input.length() - 1; i++){
-            for(int j = 0; j <= input.length() - 1; j++){
-                tempSubString = input.substring(i,i+j);
-                allSubStrings.add(tempSubString);
+
+        for (int i = 0; i <= input.length() - 1; i++) {
+            for (int j = 0; j < input.length() - 1; j++) {
+                tempSubString = input.substring(i, i + j);
+                isPalindrome(tempSubString);
             }
         }
-        return allSubStrings;
+        return count;
+    }
+
+    public boolean isPalindrome(String input) {
+        //create a string to reverse
+        String reverseString = "";
+        int valueCounter = 0;
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reverseString += input.charAt(valueCounter);
+            valueCounter++;
+        }
+        return input.equals(reverseString);
     }
 }
