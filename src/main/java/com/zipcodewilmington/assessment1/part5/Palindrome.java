4831701336c7e5c963e8ff955500f48ce5be613b
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-2.local>
2019-10-30 13:17:43 -0400

    finish Palindrome

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,32 +1,26 @@
 package com.zipcodewilmington.assessment1.part5;
 
+import com.zipcodewilmington.assessment1.part1.BasicStringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Palindrome {
-    //find all possiblesubStrings and just test them as palindromes
-    public Integer countPalindromes(String input) {
-        int count = input.length();
-        String tempSubString = "";
 
-        for (int i = 0; i <= input.length() - 1; i++) {
-            for (int j = 0; j < input.length() - 1; j++) {
-                tempSubString = input.substring(i, i + j);
-                isPalindrome(tempSubString);
+    public Integer countPalindromes(String input) {
+        int count = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j < input.length() + 1; j++) {
+                if (isPalindrome(input.substring(i, j)))
+                count++;
             }
         }
         return count;
     }
 
-    public boolean isPalindrome(String input) {
-        //create a string to reverse
-        String reverseString = "";
-        int valueCounter = 0;
 
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reverseString += input.charAt(valueCounter);
-            valueCounter++;
-        }
-        return input.equals(reverseString);
+    public static boolean isPalindrome(String input) {
+        return input.equals(BasicStringUtils.reverse(input));
     }
 }
